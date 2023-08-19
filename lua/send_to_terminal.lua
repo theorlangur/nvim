9e7c6ead2520f9c8aee7c3ab0ad5c8b53177ee1e
Dmytro Dukov <dmitry.dukov@malighting.de>
2023-08-19 10:00:13 +0200

    send_term: split, 10 lines

--- lua/send_to_terminal.lua
+++ lua/send_to_terminal.lua
@@ -4,7 +4,7 @@ local M = {
 local function get_term_channel()
   local term_bufnr = vim.fn.bufnr('^term://')
   if term_bufnr == -1 then
-    vim.cmd('split term://cmd.exe')
+    vim.cmd('belowright 10split term://cmd.exe')
     term_bufnr = vim.fn.bufnr('^term://')
   end
   return vim.bo[term_bufnr].channel
