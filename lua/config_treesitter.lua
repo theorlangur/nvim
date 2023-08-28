36088e91bb3b43296a1017efe427433892c86dbb
theorlangur <theorlangur@gmail.com>
2023-08-28 17:53:08 +0200

    adjusting treesitter
    enabled moves between arguments + made it repeatable with ;,
    removed 'tsx' from languages

--- lua/config_treesitter.lua
+++ lua/config_treesitter.lua
@@ -2,7 +2,7 @@
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'vimdoc', 'vim' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
@@ -36,6 +36,22 @@ require('nvim-treesitter.configs').setup {
         ['il'] = '@loop.inner',
       },
     },
+    move = {
+      enable = true,
+      set_jumps = true,
+      goto_next_start = {
+        [']a'] = {query="@parameter.inner", desc="Next argument"}
+      },
+      goto_previous_start = {
+        ['[a'] = {query="@parameter.inner", desc="Prev argument"}
+      }
+    }
   },
 }
-
+local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
+vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
+vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
+vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
+vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
+vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
+vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
