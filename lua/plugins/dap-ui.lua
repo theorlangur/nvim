9bdf50daffd1db52e480579f23f36b438c7fa282
Dmytro Dukov <dmitry.dukov@malighting.de>
2023-09-10 11:33:33 +0200

    configured laptop, removed commented out part

--- lua/plugins/dap-ui.lua
+++ lua/plugins/dap-ui.lua
@@ -167,47 +167,5 @@ return {
         print("Expected function got "..type(mod))
       end
     end
-
-    --[[
-    if is_win then
-      --TODO: find ms-vscode.cpptools independently of version
-      dap.adapters.cppdbg = {
-        id = 'cppdbg',
-        type = 'executable',
-        command = 'C:\\Users\\DmitryD\\.vscode\\extensions\\ms-vscode.cpptools-1.16.3-win32-x64\\debugAdapters\\bin\\OpenDebugAD7.exe',
-        options = {
-          detached = false
-        }
-      }
-
-      dap.adapters.cppvsdbg = {
-        id = 'cppvsdbg',
-        type = 'executable',
-        command = 'C:\\Users\\DmitryD\\.vscode\\extensions\\ms-vscode.cpptools-1.16.3-win32-x64\\debugAdapters\\vsdbg\\bin\\vsdbg.exe',
-        args = {
-          '--interpreter=vscode'
-        },
-        options = {
-          detached = false
-        }
-      }
-
-      dap.adapters.lldb = {
-        id = 'lldb',
-        type = 'executable',
-        command = 'c:\\Program Files\\LLVM\\bin\\lldb-vscode.exe',
-      }
-
-      dap.adapters.codelldb = {
-        id = 'codelldb',
-        type = 'server',
-        port = '3344',
-        executable = {
-          command = 'C:\\Users\\DmitryD\\.vscode\\extensions\\vadimcn.vscode-lldb-1.9.2\\adapter\\codelldb.exe',
-          args = {'--port', '3344'},
-        }
-      }
-    end
-    ]]
   end
 }
