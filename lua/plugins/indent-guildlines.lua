399b8bdbc139c72ad8b98921ee00bbbc438add7b
theorlangur <theorlangur@gmail.com>
2024-02-03 19:22:32 +0100

    adjusted indent-guidlines to v3

--- lua/plugins/indent-guildlines.lua
+++ lua/plugins/indent-guildlines.lua
@@ -3,9 +3,10 @@ return {
     'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
+    main = "ibl",
     opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
+      --char = '┊',
+      --show_trailing_blankline_indent = false,
     },
     event = {"BufReadPost", "BufNewFile"}
   }
