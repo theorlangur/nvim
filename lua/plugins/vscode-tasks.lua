5a3d81d7384307bd55932b316ad7a9b41e003dc4
theorlangur <theorlangur@gmail.com>
2024-03-01 19:14:25 +0100

    using 'powershell' for windows

--- lua/plugins/vscode-tasks.lua
+++ lua/plugins/vscode-tasks.lua
@@ -7,7 +7,11 @@ return {
   },
   config = function()
     local vstasks = require('vstask')
-    vstasks.setup({})
+    local _sh = nil
+    if vim.loop.os_uname().version:find('Windows') then
+      _sh = "powershell.exe"
+    end
+    vstasks.setup({shell=_sh})
     local ts = require('telescope')
     vim.keymap.set('n', '<leader>ta', ts.extensions.vstask.tasks, { desc = 'Run VSCode tasks' })
     vim.keymap.set('n', '<leader>ti', ts.extensions.vstask.inputs, { desc = 'Run VSCode inputs' })
