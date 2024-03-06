71bb6a5ac4aadb21905bb50adac289916460e2f6
Dmytro Dukov <dmytro.dukov@malighting.de>
2024-03-06 10:58:02 +0100

    changed/added mapping for live grepping
    sg -> sgg
    added sgr - for grepping only in resource folder
    added sgs - for grepping only in source folder

--- lua/local_configs.lua
+++ lua/local_configs.lua
@@ -1,7 +1,12 @@
 local opts_sources = {cwd="source"}
 local opts_resources = {cwd="resource"}
 local find_files = require('telescope.builtin').find_files
+local live_grep = require('telescope.builtin').live_grep
 local find_sources = function() return find_files(opts_sources) end
 local find_resources = function() return find_files(opts_resources) end
+local live_grep_sources = function() return live_grep(opts_sources) end
+local live_grep_resources = function() return live_grep(opts_resources) end
 vim.keymap.set('n', '<leader>ss', find_sources, { desc = '[S]earch in [S]ources' })
 vim.keymap.set('n', '<leader>sr', find_resources, { desc = '[S]earch in [R]esources' })
+vim.keymap.set('n', '<leader>sgr', live_grep_resources, { desc = '[S]earch by [G]rep in [R]esources' })
+vim.keymap.set('n', '<leader>sgs', live_grep_sources, { desc = '[S]earch by [G]rep in [S]ources' })
