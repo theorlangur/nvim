0af602accaae5baec761effb4ac68c3eeb3d4eab
theorlangur <theorlangur@gmail.com>
2024-03-18 10:55:01 +0100

    re-committing

--- lua/plugins/indent-guildlines.lua
+++ lua/plugins/indent-guildlines.lua
@@ -3,7 +3,7 @@ return {
     'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
-    --main = "ibl",
+    main = "ibl",
     opts = {
       --char = '┊',
       --show_trailing_blankline_indent = false,
