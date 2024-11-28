dd348bec8a7f0cb30bbbe0129b38a92fe6e75054
theorlangur <theorlangur@gmail.com>
2024-11-28 20:43:00 +0100

    added command ReClangd to re-attach clangd to all cpp buffers

--- lua/local_configs.lua
+++ lua/local_configs.lua
@@ -18,3 +18,31 @@ vim.keymap.set('n', '<leader>sgp', live_grep_projects, { desc = '[S]earch by [G]
 vim.keymap.set('v', '<leader>svr', '"zy<ESC><cmd>exec \'Telescope live_grep cwd=resource default_text=\'.escape(@z, \' \')<CR>', { desc = '[S]earch [V]isual selection in [R]esources'})
 vim.keymap.set('v', '<leader>svs', '"zy<ESC><cmd>exec \'Telescope live_grep cwd=source default_text=\'.escape(@z, \' \')<CR>', { desc = '[S]earch [V]isual selection in [S]ources'})
 vim.keymap.set('v', '<leader>svp', '"zy<ESC><cmd>exec \'Telescope live_grep cwd=projects default_text=\'.escape(@z, \' \')<CR>', { desc = '[S]earch [V]isual selection in [P]rojects'})
+
+-- Function to re-attach LSP client to all C++ buffers
+local function reattach_cpp_lsp()
+  -- Get the list of all buffers
+  local buffers = vim.api.nvim_list_bufs()
+
+  -- Iterate over all buffers
+  for _, buf in ipairs(buffers) do
+    -- Check if the buffer is of type C++
+    if vim.bo[buf].filetype == "cpp" then
+      -- Check if the buffer is valid and loaded
+      if vim.api.nvim_buf_is_valid(buf) and vim.api.nvim_buf_is_loaded(buf) then
+        -- Get the active LSP clients for the buffer
+        local clients = vim.lsp.buf_get_clients(buf)
+        if next(clients) == nil then
+          -- If no active LSP clients, re-attach the LSP server
+          vim.lsp.buf_attach_client(buf, vim.lsp.start({
+            name = "clangd", -- Change to your desired LSP client (e.g., clangd)
+          }))
+          print("Re-attached LSP client to buffer " .. buf)
+        end
+      end
+    end
+  end
+end
+
+-- Add a command to manually trigger the function
+vim.api.nvim_create_user_command("ReClangd", reattach_cpp_lsp, {})
