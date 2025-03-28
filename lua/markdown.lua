32d100299a30e26390a8a6440763fead0a70adb7
Dmytro Dukov <dmytro.dukov@malighting.de>
2025-03-28 12:56:18 +0100

    added commands for pandoc and converting md to pdf and docx

--- lua/markdown.lua
+++ lua/markdown.lua
@@ -8,5 +8,10 @@ function MarkupMappings(args)
     vim.keymap.set('v', '<Space>m', '<ESC>\'<O```<ESC>\'>o```<ESC>', {desc="Enclose visual block in```", buffer= args.buf})
     vim.keymap.set('v', '<Space>w', '<ESC>`<i`<ESC>`>la`<ESC>', {desc="Enclose visual selection in``", buffer= args.buf})
     vim.keymap.set('n', '<Space>j', ':Telescope heading<CR>', {desc="[J]ump to heading", buffer= args.buf})
+
+    vim.g.nvim_config_path = vim.fn.stdpath('config')
+    local pandoc_cfg = "--resource-path=\"..g:nvim_config_path..\"/pandoc -H listing.tex --listings -V geometry:margin=.5in"
+    vim.api.nvim_create_user_command('ToPdf', "execute \"!pandoc --from=gfm --to=pdf "..pandoc_cfg.." -o %:r.pdf %\"", {});
+    vim.api.nvim_create_user_command('ToDoc', "execute \"!pandoc --from=gfm --to=docx "..pandoc_cfg.." -o %:r.docx %\"", {});
 end
 vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, { pattern={"*.md"}, callback=MarkupMappings })
