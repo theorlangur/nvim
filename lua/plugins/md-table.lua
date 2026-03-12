93915d80ca17341d12d001fb2c4e714cd51d132d
Dmytro Dukov <dmytro.dukov@malighting.de>
2026-03-12 15:13:28 +0100

    markdown talbe support

---
+++ lua/plugins/md-table.lua
@@ -0,0 +1,10 @@
+return {
+  'SCJangra/table-nvim',
+  ft = 'markdown',
+  opts = {
+    mappings = {
+      -- insert_column_right = false,--'<A-h>',
+      -- insert_column_left = '<A-l>',
+    }
+  },
+}
