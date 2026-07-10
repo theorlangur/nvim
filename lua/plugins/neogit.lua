95aba6b06ec01a3ede0aacbf8635fd03565c99e2
TheOrlangur <theorlangur@gmail.com>
2026-07-10 20:56:22 +0200

    added neogit, disabled fugitive. to see how it looks

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,20 @@
+return {
+    -- Git related plugins
+  "NeogitOrg/neogit",
+  lazy = true,
+  dependencies = {
+    -- Only one of these is needed.
+    "sindrets/diffview.nvim",        -- optional
+    -- "esmuellert/codediff.nvim",      -- optional
+
+    -- For a custom log pager
+    "m00qek/baleia.nvim",            -- optional
+
+    -- Only one of these is needed.
+    "nvim-telescope/telescope.nvim", -- optional
+    "ibhagwan/fzf-lua",              -- optional
+    -- "nvim-mini/mini.pick",           -- optional
+    -- "folke/snacks.nvim",             -- optional
+  },
+  cmd = "G",
+}
